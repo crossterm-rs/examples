3883308581bbdcaa24c32c6a8d44f2a2858727f8
Canop <cano.petrole@gmail.com>
2019-09-29 13:01:13 +0200

    cargo fmt

--- examples/src/bin/alternate_screen_command.rs
+++ examples/src/bin/alternate_screen_command.rs
@@ -3,44 +3,42 @@
 //! unbuffered API.
 
 use std::{
-    thread,
-    time,
     io::{stdout, Write},
+    thread, time,
 };
 
 use crossterm::{
-    AlternateScreen,
-    Clear, ClearType,
-    Color,
-    Crossterm,
-    Goto,
-    Output,
-    PrintStyledFont,
-    queue,
-    Result,
-    style,
+    queue, style, AlternateScreen, Clear, ClearType, Color, Crossterm, Goto, Output,
+    PrintStyledFont, Result,
 };
 
 fn print_wait_screen() -> Result<()> {
     let mut stdout = stdout();
     queue!(stdout, Clear(ClearType::All))?;
     queue!(stdout, Goto(0, 0))?;
-    queue!(stdout, Output(
-        "Welcome to the wait screen.\n\
-         Please wait a few seconds until we arrive back at the main screen.\n\
-         Progress: ".to_string(),
-    ))?;
+    queue!(
+        stdout,
+        Output(
+            "Welcome to the wait screen.\n\
+             Please wait a few seconds until we arrive back at the main screen.\n\
+             Progress: "
+                .to_string(),
+        )
+    )?;
     stdout.flush()?;
     // print some progress example.
     let items = 5;
     for i in 1..=items {
         // print the current counter at the line of `Seconds to Go: {counter}`
         queue!(stdout, Goto(10, 2))?;
-        queue!(stdout, PrintStyledFont(
-            style(format!("{} of the {} items processed", i, items))
-                .with(Color::Red)
-                .on(Color::Blue)
-        ))?;
+        queue!(
+            stdout,
+            PrintStyledFont(
+                style(format!("{} of the {} items processed", i, items))
+                    .with(Color::Red)
+                    .on(Color::Blue)
+            )
+        )?;
         stdout.flush()?;
         // 1 second delay
         thread::sleep(time::Duration::from_secs(1));
@@ -62,4 +60,3 @@ fn print_wait_screen_on_alternate_window() -> Result<()> {
 fn main() -> Result<()> {
     print_wait_screen_on_alternate_window()
 }
-
